421a50b58b4442dfac3320371b030ace6d27bda8
Victorsward <59231792+Victorsward32@users.noreply.github.com>
2024-01-05 01:08:47 +0530

    commit

--- Linkedlist/Main.java
+++ Linkedlist/Main.java
@@ -1,25 +1,39 @@
 package Linkedlist;
-import java.util.*;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
         //linked list
-           objects a=;
-           int b=30;
-           int c=sc.nextInt();
+        int n=5;
+        for (int i = 1; i <= n-1; i++) {
+            for (int s = n; s >= i; s--) {
+                System.out.print(" ");
+                
+            }
+            for (int j = 1; j <=i; j++) {
+                System.out.print("*");
+                
+            }
+            System.out.println();
+            
+        }
 
-           if(a>c || c<b){
-
-            System.out.println("in between them");
-
-           }
-           else{
-            System.out.println("error");
-           }
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= i; s++) {
+                System.out.print(" ");
+                
+            }
+            for (int j = n; j >=i; j--) {
+                System.out.print("*");
+                
+            }
+            System.out.println();
+            
+        }
         
-    }
+    
 
 }
-class date{
-    int date;
 }
+
--- Sort/Main.java
+++ Sort/Main.java
@@ -1,12 +1,23 @@
 import java.util.Arrays;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int []arr={1,34,89,54,2,5};
+         int size=5;
+        Scanner sc= new Scanner(System.in);
+        int []arr=new int[size];
+        for (int l=0;l<size ;l++){
+            arr[l]=sc.nextInt();
+        }
+       
+
+        int arr2[]=Arrays.copyOf(arr,arr.length);
+        System.out.println(Arrays.toString(arr2));
+        int temp2=0;
         int temp=0;
         for(int i=0 ; i<arr.length; i++){
             for (int j = i+1; j < arr.length; j++) {
-                if(arr[i] < arr[j]){
+                if(arr[i] > arr[j]){
                 temp=arr[i];
                 arr[i]=arr[j];
                 arr[j]=temp; 
@@ -14,11 +25,29 @@ public class Main {
             
             }
             
-
         }
-        System.out.println(Arrays.toString(arr));
-        // second higest no is 
+        
+        // System.out.println(Arrays.toString(arr));
+        // second higest no is 7
         System.out.println("second largest element"+" :- "+arr[1]);
+    
+        // Finding the smallest elemnt in array
+        // for (int a = 0; a < arr.length; a++) {
+        //     for(int b = 0; b < arr.length; b++){
+        //         if(arr[a]>arr[b+1]){
+        //             temp2=arr[a];
+        //             arr[a]=arr[b];
+        //             arr[b]=temp2;
+
+        //         }
+
+                
+        //     }
+
+            
+        // }
+        //  System.out.println(arr2[1]);
+    
     }
     
 }
