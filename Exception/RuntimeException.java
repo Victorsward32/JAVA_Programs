a1fa7a9a361adb0cb4695733579cd0215c95015a
Victorsward <59231792+Victorsward32@users.noreply.github.com>
2024-01-08 20:20:14 +0530

    commit

---
+++ Exception/RuntimeException.java
@@ -0,0 +1,22 @@
+package Exception;
+// Its the example of the runtime exeception
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class RuntimeException {
+    public static void main(String[] args)  {
+         try {
+            FileInputStream file=new FileInputStream("C:\\Users\\Sumit Jadhav\\Documents\\GitHub\\JAVA_Programs\\interview\\armstrong.java");
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        //rthis is also a form of unchecked exception 
+        // int a=100,b=0, c;
+        // c=a/b;
+        // System.out.println("Realtime Exception or Unchecked Exception"+c);
+    }
+    
+}
---
+++ Exception/compiletime.java
@@ -0,0 +1,11 @@
+package Exception;
+
+import java.io.FileInputStream;
+
+// its the example of Compiletime Exeption
+public class compiletime {
+    public static void main(String[] args) {
+        FileInputStream file=new FileInputStream("C:\\Users\\Sumit Jadhav\\Documents\\GitHub\\JAVA_Programs\\interview\\armstrong.java");
+    }
+    
+}
